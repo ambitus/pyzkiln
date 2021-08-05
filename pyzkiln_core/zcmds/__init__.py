b4d6b0e4bd8336cb26652890318756237d9db562
Frank De Gilio <fdegilio@gmail.com>
2021-08-05 11:20:01 -0400

    Update getASM to get_asm and make it more pythonic

--- pyzkiln_core/zcmds/__init__.py
+++ pyzkiln_core/zcmds/__init__.py
@@ -1,9 +1,9 @@
 # submodules that will be imported to pyzkiln_core package
-__all__ = ["display_active_jobs", "getASM", "getParmlib",
+__all__ = ["display_active_jobs", "get_asm", "getParmlib",
            "iplinfo"]
 
 from pyzkiln_core.zcmds.display_active_jobs import *
-from pyzkiln_core.zcmds.getASM import *
+from pyzkiln_core.zcmds.get_asm import *
 from pyzkiln_core.zcmds.getParmlib import *
 from pyzkiln_core.zcmds.iplinfo import *
 
