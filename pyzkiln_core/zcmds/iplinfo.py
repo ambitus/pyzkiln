bfb3bbc293671d75cf958be1c9cd1a1f06ab917d
Frank De Gilio <degilio@us.ibm.com>
2022-12-12 14:42:35 -0500

    Simplified code
    Signed-off-by: Frank De Gilio <degilio@us.ibm.com>

--- pyzkiln_core/zcmds/iplinfo.py
+++ pyzkiln_core/zcmds/iplinfo.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-import sys
-import os
-import argparse
-from zoautil_py import opercmd
-'''
+"""
 iplinfo
 return the IPL info of the z/OS system as a dictionary.
 
@@ -29,117 +25,122 @@ IEE254I. That will generate a dictionary with the following keys:
 If you run iplinfo with a sysparm, it will return a dictionary
 with the following Keys
  Parameter - The parameter queried
- State - The state of that parameter
-'''
+ State - The sate of that paramter
+"""
+import sys
+import os
+import argparse
+from zoautil_py import opercmd
 
 
 def iplinfo(sysparm=None):
-    '''
-    iplinfo function:
-    Take the input and return a dictionary based on the input. The Current
-    supported inputs are none or a sysparm to be queried
-    '''
+    """
+    Format the ipl data from the operator command.
+    The Current supported inputs are none or a sysparm to be queried
+    """
+    # define a dictionary in which the data will be placed
+    return_dictionary = {}
     if sysparm is None:
-        cmdData = opercmd.execute("DISPLAY IPLINFO").to_dict()
+        cmd_data = opercmd.execute("DISPLAY IPLINFO").to_dict()
     else:
-        cmdData = opercmd.execute(f"DISPLAY IPLINFO,{sysparm}").to_dict()
+        cmd_data = opercmd.execute(f"DISPLAY IPLINFO,{sysparm}").to_dict()
 
-    if cmdData["rc"] > 0:
-        print(f"Data not Retrieved. Return code is: {cmdData['rc']}")
-        return cmdData
+    if cmd_data["rc"] > 0:
+        print(f"Data not Retrieved. Return code is: {cmd_data['rc']}")
+        return cmd_data
 
-    cmdresponse = cmdData['stdout_response']
+    cmdresponse = cmd_data["stdout_response"]
     if "IEE254I" in cmdresponse:
         cmdlist = cmdresponse.split("\n")
-        time = date = release = license = used = indata = ondata = ""
-        archlvl = mltshr = ieasym = ieasys = iodfcurr = iodforigin = ""
-        iplorigin = iplcurr = iplvol = ""
         for line in cmdlist:
             if "SYSTEM IPLED" in line:
-                time = line[line.find("AT")+3:line.find("AT")+11]
-                date = line[line.find("ON")+3:]
+                return_dictionary["IPL Time"] = line[
+                    line.find("AT") + 3 : line.find("AT") + 11
+                ]
+                return_dictionary["IPL Date"] = line[line.find("ON") + 3 :]
 
-            if "RELEASE" in line:
-                release = line[line.find("RELEASE")+8:
-                               line.find("LICENSE")-1].strip()
-                license = line[line.find("=")+2:]
+            elif "RELEASE" in line:
+                return_dictionary["Release"] = line[
+                    line.find("RELEASE") + 8 : line.find("LICENSE") - 1
+                ].strip()
+                return_dictionary["License"] = line[line.find("=") + 2 :]
 
-            if "USED" in line:
-                used = line[line.find("USED")+5:line.find("IN")-1].strip()
-                indata = line[line.find("IN")+3:line.find("ON")-1].strip()
-                ondata = line[line.find("ON")+3:].strip()
+            elif "USED" in line:
+                return_dictionary["Member Used"] = line[
+                    line.find("USED") + 5 : line.find("IN") - 1
+                ].strip()
+                return_dictionary["Dataset"] = line[
+                    line.find("IN") + 3 : line.find("ON") - 1
+                ].strip()
+                return_dictionary["Device"] = line[line.find("ON") + 3 :].strip()
 
-            if "ARCHLVL" in line:
-                archlvl = line[line.find("ARCHLVL =")+10:
-                               line.find("MTL")].strip()
-                mltshr = line[line.find("MTLSHARE =")+11:].strip()
+            elif "ARCHLVL" in line:
+                return_dictionary["Arch Level"] = line[
+                    line.find("ARCHLVL =") + 10 : line.find("MTL")
+                ].strip()
+                return_dictionary["MLT Share"] = line[
+                    line.find("MTLSHARE =") + 11 :
+                ].strip()
 
-            if "IEASYM" in line:
-                ieasym = line[line.find("(")+1:line.find(")")]
+            elif "IEASYM" in line:
+                return_dictionary["IEASYM List"] = line[
+                    line.find("(") + 1 : line.rfind(")")
+                ]
 
-            if "IEASYS" in line:
-                ieasys = line[line.find("("):]
+            elif "IEASYS" in line:
+                return_dictionary["IEASYS List"] = line[line.find("(") :]
 
-            if "IODF" in line:
-                iodforigin = line[line.find("ORIGINAL(")+9:line.find(")")]
-                iodfcurr = line[line.find("CURRENT(")+8:line.find(")",
-                                line.find(")")+1)]
+            elif "IODF" in line:
+                return_dictionary["Original IODF"] = line[
+                    line.find("ORIGINAL(") + 9 : line.find(")")
+                ]
+                return_dictionary["Current IODF"] = line[
+                    line.find("CURRENT(") + 8 : line.find(")", (line.find(")") + 1))
+                ]
 
-            if "IPL DEVICE" in line:
-                iplorigin = line[line.find("ORIGINAL(")+9:line.find(")")]
-                iplcurr = line[line.find("CURRENT(")+8:line.find(")",
-                               line.find(")")+1)]
-                iplvol = line[line.find("VOLUME(")+7:line.rfind(")")]
-
-        return_dictionary = {"IPL Time": time, "IPL Date": date,
-                             "Release": release, "License": license,
-                             "Member Used": used, "Dataset": indata,
-                             "Device": ondata, "Arch Level": archlvl,
-                             "MLT Share": mltshr, "IEASYM List": ieasym,
-                             "IEASYS List": ieasys,
-                             "Original IODF": iodforigin,
-                             "Current IODF": iodfcurr,
-                             "Original IPL Device": iplorigin,
-                             "Current IPL Device": iplcurr,
-                             "IPL Volume": iplvol}
+            elif "IPL DEVICE" in line:
+                return_dictionary["Original IPL Device"] = line[
+                    line.find("ORIGINAL(") + 9 : line.find(")")
+                ]
+                return_dictionary["Current IPL Device"] = line[
+                    line.find("CURRENT(") + 8 : line.find(")", line.find(")") + 1)
+                ]
+                return_dictionary["IPL Volume"] = line[
+                    line.find("VOLUME(") + 7 : line.rfind(")")
+                ]
 
     if "IEE255I" in cmdresponse:
-        parm = cmdresponse[cmdresponse.find("'")+1:cmdresponse.rfind("'")]
-        state = cmdresponse[cmdresponse.rfind(":")+1:].strip()
-        return_dictionary = {"Parameter": parm, "State": state}
+        return_dictionary["Parameter"] = cmdresponse[
+            cmdresponse.find("'") + 1 : cmdresponse.rfind("'")
+        ]
+        return_dictionary["State"] = cmdresponse[cmdresponse.rfind(":") + 1 :].strip()
 
     return return_dictionary
 
 
 def parse_args(argv=None):
-    '''
-    This function is responsible for handling arguments. It relies on
-    the argparse module.
-    '''
+    """
+    Handle the arguments.
+    It relies on the argparse module.
+    """
     program_name = os.path.basename(sys.argv[0])
 
     if argv is None:
         argv = sys.argv[1:]
 
-    try:
         parser = argparse.ArgumentParser(program_name)
-        parser.add_argument("-s", "--sysparm", default=None,
-                            help="A sysparm you want to query.")
-        opts = parser.parse_args(argv)
-        return opts
-
-    except Exception as e:
-        indent = len(program_name) * " "
-        sys.stderr.write(program_name + ": " + repr(e) + "\n")
-        sys.stderr.write(indent + "  for help use --help")
-        sys.exit(1)
+        parser.add_argument(
+            "-s", "--sysparm", default=None, help="A sysparm you want to query."
+        )
+    opts = parser.parse_args(argv)
+    return opts
 
 
 def main():
-    '''
+    """
     main function:
     gather parms and call the iplinfo function
-    '''
+    """
     args = parse_args()
     print(iplinfo(args.sysparm))
 
