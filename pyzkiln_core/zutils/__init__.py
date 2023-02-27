cef082af3de1462d7d69f3bc426ed5088f622200
Frank De Gilio <degilio@us.ibm.com>
2023-02-27 09:26:23 -0500

    Updated documentation
    Singed-off-by: Frank De Gilio degilio@us.ibm.com
    Fixed the doc and updated init.py
    
    Signed-off-by: Frank De Gilio <degilio@us.ibm.com>

--- pyzkiln_core/zutils/__init__.py
+++ pyzkiln_core/zutils/__init__.py
@@ -1,9 +1,10 @@
 # submodules that will be imported to pyzkiln_core package
-__all__ = ["joblist","findActiveJobs","zfsadm","fsutils","runrexx"]
+__all__ = ["joblist","findActiveJobs","zfsadm","fsutils","runrexx","create_sysin"]
 
 from pyzkiln_core.zutils.findActiveJobs import *
 from pyzkiln_core.zutils.joblist import *
 from pyzkiln_core.zutils.zfsadm import *
 from pyzkiln_core.zutils.fsutils import * 
 from pyzkiln_core.zutils.runrexx import * 
+from pyzkiln_core.zutils.create_sysin import *
  
